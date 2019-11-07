e386c257d6184478617bf950685cd71dbc6319db
Hi <Hi@Hello>
2019-11-07 04:26:44 -0800

    first commit

---
+++ javaproject/src/browsers/Arraysexample.java
@@ -0,0 +1,34 @@
+package browsers;
+
+public class Arraysexample {
+	
+	public static void main(String[] args) {
+		
+		int arr[]=new int[8];
+		
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		arr[3]=40;
+		arr[4]=50;
+		
+		for (int x = 0; x < arr.length; x++) {
+			System.out.println(arr[x]);	
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+	
+	
+	
+	
+	
+	}
+	
+
+}
