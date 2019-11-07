ab72248a802a1325be025b8890e257cff6b26007
Hi <Hi@Hello>
2019-11-07 04:27:32 -0800

    First commit

---
+++ javaproject/src/practice/Employee.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Employee {
+
+	
+	public String empname;
+	public String empsalary;
+	public String empdesg;
+	
+	
+	public static String empcomp="IBM";
+	
+	
+	
+	
+	
+}
